9fc51f711feabfa59e238cee46767753066e3e39
Robert Hieger <rhieger@gmail.com>
2022-09-16 18:06:56 -0400

    Complete Challenge #1: count 1-10 with for loop

---
+++ challenge-1/challengeHere.js
@@ -0,0 +1,11 @@
+// Challenge #1
+//
+// Print numbers from 1 to 10 with a for loop.
+
+// MY SOLUTION
+
+console.log('\n\nLet\'s count fromn 1 to 10.\n');
+
+for (let count = 1; count <= 10; count++) {
+    console.log(count);
+}
